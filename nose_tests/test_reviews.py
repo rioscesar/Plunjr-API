001aa32471804b3ea69cd3005d75a629d97f1b92
Cesar Rios <rios_cesar@utexas.edu>
2016-04-04 19:16:04 -0500

    leaving only post tests

--- nose_tests/test_reviews.py
+++ nose_tests/test_reviews.py
@@ -8,30 +8,30 @@ class ReviewAPITest(NDTestCase):
     def setUp(self):
         super(ReviewAPITest, self).setUp()
 
-    def test_get(self):
-        get_uri = '{}/reviews/{}'.format(app.config['BASE_URL'], BEST_IN_THE_WORLD)
-        response = self.json_get(get_uri)
-
-        reponse_dict = self.dict_from_response(response)
-
-        self.assertEqual(response.status_code, app.config['OK'])
-
-    def test_get_all(self):
-        get_uri = '{}/restrooms/{}/reviews/'.format(app.config['BASE_URL'], CHRIS)
-        response = self.json_get(get_uri)
-
-        reponse_dict = self.dict_from_response(response)
-
-        self.assertEqual(response.status_code, app.config['OK'])
-
-        get_uri = '{}/restrooms/{}/reviews'.format(app.config['BASE_URL'], CHRIS)
-        response = self.json_get(get_uri)
-
-        reponse_dict_cmp = self.dict_from_response(response)
-
-        self.assertEqual(response.status_code, app.config['OK'])
-
-        self.assertEqual(reponse_dict, reponse_dict_cmp)
+    # def test_get(self):
+    #     get_uri = '{}/reviews/{}'.format(app.config['BASE_URL'], BEST_IN_THE_WORLD)
+    #     response = self.json_get(get_uri)
+    #
+    #     reponse_dict = self.dict_from_response(response)
+    #
+    #     self.assertEqual(response.status_code, app.config['OK'])
+    #
+    # def test_get_all(self):
+    #     get_uri = '{}/restrooms/{}/reviews/'.format(app.config['BASE_URL'], CHRIS)
+    #     response = self.json_get(get_uri)
+    #
+    #     reponse_dict = self.dict_from_response(response)
+    #
+    #     self.assertEqual(response.status_code, app.config['OK'])
+    #
+    #     get_uri = '{}/restrooms/{}/reviews'.format(app.config['BASE_URL'], CHRIS)
+    #     response = self.json_get(get_uri)
+    #
+    #     reponse_dict_cmp = self.dict_from_response(response)
+    #
+    #     self.assertEqual(response.status_code, app.config['OK'])
+    #
+    #     self.assertEqual(reponse_dict, reponse_dict_cmp)
 
     def test_post_review(self):
         uri = '{}/reviews'.format(app.config['BASE_URL'])
